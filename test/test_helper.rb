808193de24518b0e2da0c1a635d0bcc84f47a8de
dgarciaap <dgarcia24@ucol.mx>
2020-03-23 17:56:01 -0600

    [ADD] filters for coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,9 @@ SimpleCov.start 'rails' do
   add_filter '/spec/'
   add_filter '/config/'
   add_filter '/vendor/'
+  add_filter '/channels'
+  add_filter '/jobs'
+  add_filter '/helpers'
   add_group 'Controllers', 'app/controllers'
   add_group 'Models', 'app/models'
 
