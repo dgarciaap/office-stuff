60195883a01cb76ef8f23012deff44789b82e7f0
dgarciaap <dgarcia24@ucol.mx>
2020-03-28 14:31:38 -0600

    [DEL] comments

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,9 +10,9 @@ SimpleCov.start 'rails' do
   add_filter 'app/controllers/coverages_controller.rb'
   add_group 'Controllers', 'app/controllers'
   add_group 'Models', 'app/models'
-  add_group "Services", 'app/services'
+  add_group 'Services', 'app/services'
 end
-#SimpleCov.root("public")
+SimpleCov.root("public")
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 #require 'sidekiq/testing'
@@ -20,31 +20,3 @@ require 'rails/test_help'
 
 knapsack_pro_adapter = KnapsackPro::Adapters::MinitestAdapter.bind
 knapsack_pro_adapter.set_test_helper_path(__FILE__)
-#require 'knapsack_pro'
-
-#ENV['RAILS_ENV'] ||= 'test'
-#require 'simplecov'
-##require 'sidekiq/testing'
-##require 'rails/test_help'
-
-#knapsack_pro_adapter = KnapsackPro::Adapters::MinitestAdapter.bind
-#knapsack_pro_adapter.set_test_helper_path(__FILE__)
-
-
-#SimpleCov.start 'rails' do
-  #SimpleCov.coverage_dir 'public/coverage'
-  #add_filter '/spec/'
-  #add_filter '/config/'
-  #add_filter '/vendor/'
-  #add_filter '/channels'
-  #add_filter '/helpers'
-  #add_group 'Controllers', 'app/controllers'
-  #add_group 'Models', 'app/models'
-  #add_group 'Services', 'app/services'
-
-  #SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
-#end
-
-#SimpleCov.at_exit do
-  #SimpleCov.result.format!
-#end
