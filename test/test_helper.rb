1df28901aeda21e6807e4776a896d19c1d8cec2b
dgarciaap <dgarcia24@ucol.mx>
2020-03-28 12:01:28 -0600

    [FIX] knapsack config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,10 @@ require 'simplecov'
 require 'sidekiq/testing'
 require 'knapsack_pro'
 
+knapsack_pro_adapter = KnapsackPro::Adapters::MinitestAdapter.bind
+knapsack_pro_adapter.set_test_helper_path(__FILE__)
+
+
 SimpleCov.start 'rails' do
   SimpleCov.coverage_dir 'public/coverage'
   add_filter '/spec/'
@@ -14,12 +18,8 @@ SimpleCov.start 'rails' do
   add_group 'Models', 'app/models'
   add_group 'Services', 'app/services'
 
-  knapsack_pro_adapter = KnapsackPro::Adapters::MinitestAdapter.bind
-  knapsack_pro_adapter.set_test_helper_path(__FILE__)
-
   SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 end
-
 
 SimpleCov.at_exit do
   SimpleCov.result.format!
