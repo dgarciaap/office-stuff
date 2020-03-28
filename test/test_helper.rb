0f7d6fc68e5da08bbc7bfdc91903f6944dea1c52
dgarciaap <dgarcia24@ucol.mx>
2020-03-28 14:07:02 -0600

    [FIX] test helper file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,28 +1,50 @@
+require 'simplecov'
 require 'knapsack_pro'
 
+SimpleCov.start 'rails' do
+  filters.clear # This will remove the :root_filter and :bundler_filter that come via simplecov's defaults
+  add_filter '/vendor/'
+  add_filter '/config/'
+  add_filter 'app/channels'
+  add_filter 'app/helpers'
+  add_filter 'app/controllers/coverages_controller.rb'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group "Services", 'app/services'
+end
+#SimpleCov.root("public")
 ENV['RAILS_ENV'] ||= 'test'
-require 'simplecov'
+require_relative '../config/environment'
 #require 'sidekiq/testing'
-#require 'rails/test_help'
+require 'rails/test_help'
 
 knapsack_pro_adapter = KnapsackPro::Adapters::MinitestAdapter.bind
 knapsack_pro_adapter.set_test_helper_path(__FILE__)
+#require 'knapsack_pro'
+
+#ENV['RAILS_ENV'] ||= 'test'
+#require 'simplecov'
+##require 'sidekiq/testing'
+##require 'rails/test_help'
+
+#knapsack_pro_adapter = KnapsackPro::Adapters::MinitestAdapter.bind
+#knapsack_pro_adapter.set_test_helper_path(__FILE__)
 
 
-SimpleCov.start 'rails' do
-  SimpleCov.coverage_dir 'public/coverage'
-  add_filter '/spec/'
-  add_filter '/config/'
-  add_filter '/vendor/'
-  add_filter '/channels'
-  add_filter '/helpers'
-  add_group 'Controllers', 'app/controllers'
-  add_group 'Models', 'app/models'
-  add_group 'Services', 'app/services'
+#SimpleCov.start 'rails' do
+  #SimpleCov.coverage_dir 'public/coverage'
+  #add_filter '/spec/'
+  #add_filter '/config/'
+  #add_filter '/vendor/'
+  #add_filter '/channels'
+  #add_filter '/helpers'
+  #add_group 'Controllers', 'app/controllers'
+  #add_group 'Models', 'app/models'
+  #add_group 'Services', 'app/services'
 
-  SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
-end
+  #SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+#end
 
-SimpleCov.at_exit do
-  SimpleCov.result.format!
-end
+#SimpleCov.at_exit do
+  #SimpleCov.result.format!
+#end
